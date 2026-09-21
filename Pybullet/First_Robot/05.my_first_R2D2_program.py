224d5b244c9b7c154437230b809f4b88f9009f2b
ThandoP <thaforex01@gmail.com>
2026-09-21 20:43:14 +0200

    Make a spinning robot

--- Pybullet/First_Robot/05.my_first_R2D2_program.py
+++ Pybullet/First_Robot/05.my_first_R2D2_program.py
@@ -27,3 +27,26 @@ for joint_index in range(num_joints):
     joint_info = pyb.getJointInfo(robotId, joint_index)
     print("Joint Index:", joint_info[0:2])  # Print the first 15 elements of joint_info for brevity
 
+# use list to have right wheel joints 2,3 & left wheel joints 6,7 
+joint_indices = [2, 3, 6, 7]
+
+"""
+2 - Right wheel joint 1
+3 - Right wheel joint 2
+6 - Left wheel joint 1
+7 - Left wheel joint 2
+"""
+maxforce = 10  # Maximum force to apply to the joints
+mode = pyb.VELOCITY_CONTROL  # Control mode for the joints
+
+pyb.setJointMotorControlArray(robotId, joint_indices, controlMode=mode,targetVelocities=[20, 10, 10, 10], forces=[maxforce]*len(joint_indices))  # Set the target velocities for the joints 
+
+
+
+#To control the simulation, we can use a loop to step through the simulation and apply forces or control the robot'  joints. For this example, we will just run the simulation for a few seconds.
+
+for i in range(24000):  # Run the simulation for 2400 steps (approximately 10 seconds at 240 Hz)
+    pyb.stepSimulation()  # Step the simulation
+    time.sleep(1./240.)  # Sleep to match real-time (assuming 240 Hz simulation frequency)
+
+    pyb.disconnect()  # Disconnect from the PyBullet server when done   
